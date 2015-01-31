06b1456d33a28a54c014e0c502bd0234843b5c3b
wcebron <wcebron@gmail.com>
2015-01-30 16:53:26 -0800

    Updated for firebase

--- JS/tictactoeangular.js
+++ JS/tictactoeangular.js
@@ -1,60 +1,119 @@
-var app = angular.module('ticApp', []);
+var app = angular.module('ticApp', ["firebase"]);
 
-app.controller('ticCtrl', function($scope){
-	$scope.board = ['','','','','','','','',''];
-	
-	$scope.counter = 0;
+app.controller('ticCtrl', function($scope, $firebase){
+	var ref = new Firebase("https://tictactoeangular.firebaseio.com/");
+    // creates an AngularFire reference to the data
+    var sync = $firebase(ref);
+    // creates a synchronized array for use in our HTML code
 
-	$scope.winningCombo = [
-		[0, 1, 2],
-		[3, 4, 5],
-		[6, 7, 8],
-		[0, 3, 6],
-		[1, 4, 7],
-		[2, 5, 8],
-		[2, 4, 6],
-		[0, 4, 8],
-	];
+
+    var boardRef = ref.child('board');  //creates a child ref for the board in firebase
+    var boardSync = $firebase(boardRef); //creates a synchronized object for board
+    $scope.board = boardSync.$asArray(); //creates an array for each board data
+
+    var counterRef = ref.child('counter'); //creates a child ref for the counter in firebase
+   	var counterSync = $firebase(counterRef);  //creates a synchronized object for counter
+    $scope.counter = counterSync.$asArray(); //creates an array for the counter data
+
+		var victorRef = ref.child('victor');
+		var victorSync = $firebase(victorRef);
+		$scope.victor = sync.$asObject()
+		$scope.victor.checkWinner = false;
+		$scope.victor.$save();
+
+	$scope.board.$loaded(function(){
+  		if ($scope.board.length === 0){
+  			for(var i = 0; i < 9; i++){
+  				$scope.board.$add({marker: ""}); //creates board wiht nine tiles because it doesn't exist
+  			}
+  		}
+  		else{
+  			for(i = 0; i < 9; i++){
+  				$scope.board[i].marker =""; //empties an existing board by iterating through each piece and marking it blank
+  				$scope.board.$save(i);  //Saves board data to specified Firebase reference
+  			}
+  		}
+  	});
+
+  	$scope.counter.$loaded(function(){
+  		if($scope.counter.length === 0){  //essentially asking if counter does not exist
+  			$scope.counter.$add({move: 0}); //creates counter in Firebase with a move key and value of zero
+  		}
+  		else{
+  			$scope.counter[0].move = 0; //if counter exists, set it move node to zero locally
+  			$scope.counter.$save(0); //then save the
+
+  		}
+  	});
+
+
+	// $scope.board = ['','','','','','','','',''];
+
+	// $scope.counter = 0;
+
+	// $scope.winningCombo = [
+	// 	[0, 1, 2],
+	// 	[3, 4, 5],
+	// 	[6, 7, 8],
+	// 	[0, 3, 6],
+	// 	[1, 4, 7],
+	// 	[2, 5, 8],
+	// 	[2, 4, 6],
+	// 	[0, 4, 8],
+	// ];
 
 	$scope.runGame = function(index){
-		if(($scope.board[index]) !== "X" && ($scope.board[index]) !== "O" && ($scope.counter % 2 == 0)){
-		$scope.board[index] = "X"; //adds x to the board at the indexed position
-		$scope.checkWin(); //runs win condition function
-		$scope.counter++; //increments counter by one
-		console.log($scope.board);
+
+		if($scope.victor.checkWinner === true){
+			return false;
 		}
 
-		else if (($scope.board[index]) !== "X" && ($scope.board[index]) !== "O"){ 
-		$scope.board[index] = "O";
+		if(($scope.board[index].marker !== "X") && ($scope.board[index].marker !== "O") && ($scope.counter[0].move % 2 == 0)){
+		$scope.board[index].marker = "X" //adds x to the board in firebase (won't work with above)
+		$scope.board.$save(index);
+		$scope.checkWin(); //runs win condition function
+		$scope.counter[0].move++; //increments counter by one
+		$scope.counter.$save(0);
+		// console.log($scope.board[index]);
+		}
+
+		else if (($scope.board[index].marker !== "X") && ($scope.board[index].marker !== "O")){
+		$scope.board[index].marker = "O";
+		$scope.board.$save($scope.board[index]);
 		$scope.checkWin();
-		$scope.counter++;
-		console.log($scope.board);
+		$scope.counter[0].move++;
+		$scope.counter.$save(0);
+		// console.log($scope.board[index]);
 		}
 	};
 
 	$scope.checkWin = function(){
-		if (($scope.board[0] == "X" && $scope.board[1] == "X" && $scope.board[2] == "X") ||
-			($scope.board[3] == "X" && $scope.board[4] == "X" && $scope.board[5] == "X") || 
-			($scope.board[6] == "X" && $scope.board[7] == "X" && $scope.board[8] == "X") || 
-			($scope.board[0] == "X" && $scope.board[3] == "X" && $scope.board[6] == "X") || 
-			($scope.board[1] == "X" && $scope.board[4] == "X" && $scope.board[7] == "X") || 
-			($scope.board[2] == "X" && $scope.board[5] == "X" && $scope.board[8] == "X") || 
-			($scope.board[2] == "X" && $scope.board[4] == "X" && $scope.board[6] == "X") ||
-			($scope.board[0] == "X" && $scope.board[4] == "X" && $scope.board[8] == "X"))
+		if (($scope.board[0].marker == "X" && $scope.board[1].marker == "X" && $scope.board[2].marker == "X") ||
+			($scope.board[3].marker == "X" && $scope.board[4].marker == "X" && $scope.board[5].marker == "X") ||
+			($scope.board[6].marker == "X" && $scope.board[7].marker == "X" && $scope.board[8].marker == "X") ||
+			($scope.board[0].marker == "X" && $scope.board[3].marker == "X" && $scope.board[6].marker == "X") ||
+			($scope.board[1].marker == "X" && $scope.board[4].marker == "X" && $scope.board[7].marker == "X") ||
+			($scope.board[2].marker == "X" && $scope.board[5].marker == "X" && $scope.board[8].marker == "X") ||
+			($scope.board[2].marker == "X" && $scope.board[4].marker == "X" && $scope.board[6].marker == "X") ||
+			($scope.board[0].marker == "X" && $scope.board[4].marker == "X" && $scope.board[8].marker == "X"))
 		{
 			$scope.blaiseWins = true;
+			$scope.victor.checkWinner = true;
+			$scope.victor.$save();
 
 		}
-		else if (($scope.board[0] == "O" && $scope.board[1] == "O" && $scope.board[2] == "O") ||
-			($scope.board[3] == "O" && $scope.board[4] == "O" && $scope.board[5] == "O") || 
-			($scope.board[6] == "O" && $scope.board[7] == "O" && $scope.board[8] == "O") || 
-			($scope.board[0] == "O" && $scope.board[3] == "O" && $scope.board[6] == "O") || 
-			($scope.board[1] == "O" && $scope.board[4] == "O" && $scope.board[7] == "O") || 
-			($scope.board[2] == "O" && $scope.board[5] == "O" && $scope.board[8] == "O") || 
-			($scope.board[2] == "O" && $scope.board[4] == "O" && $scope.board[6] == "O") ||
-			($scope.board[0] == "O" && $scope.board[4] == "O" && $scope.board[8] == "O"))
+		else if (($scope.board[0].marker == "O" && $scope.board[1].marker == "O" && $scope.board[2].marker == "O") ||
+			($scope.board[3].marker == "O" && $scope.board[4].marker == "O" && $scope.board[5].marker == "O") ||
+			($scope.board[6].marker == "O" && $scope.board[7].marker == "O" && $scope.board[8].marker == "O") ||
+			($scope.board[0].marker == "O" && $scope.board[3].marker == "O" && $scope.board[6].marker == "O") ||
+			($scope.board[1].marker == "O" && $scope.board[4].marker == "O" && $scope.board[7].marker == "O") ||
+			($scope.board[2].marker == "O" && $scope.board[5].marker == "O" && $scope.board[8].marker == "O") ||
+			($scope.board[2].marker == "O" && $scope.board[4].marker == "O" && $scope.board[6].marker == "O") ||
+			($scope.board[0].marker == "O" && $scope.board[4].marker == "O" && $scope.board[8].marker == "O"))
 		{
 			$scope.zachWins = true;
+			$scope.victor.checkWinner = true;
+			$scope.victor.$save();
 		}
 		else if ($scope.counter == 8) {
 			alert("Looks like a tie!");
